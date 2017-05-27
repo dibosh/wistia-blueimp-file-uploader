ee363d744a99b0270a57bcf2557c4d82159b6936
dibosh <dibosh@newscred.com>
2017-05-27 14:36:18 +0600

    Complete the wistia video uploader component

--- src/app/wistia-video-uploader/wistia-video-uploader.component.ts
+++ src/app/wistia-video-uploader/wistia-video-uploader.component.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-wistia-video-uploader',
   templateUrl: './wistia-video-uploader.component.html',
   styleUrls: ['./wistia-video-uploader.component.scss']
 })
-export class WistiaVideoUploaderComponent implements OnInit {
+export class WistiaVideoUploaderComponent implements AfterViewInit {
   uploadedVideoUrl: string;
   isVideoUploaded: boolean;
+  isUploadingVideo: boolean;
+  videoUploadProgress: number;
+  apiSecret: string;
 
-  constructor() {}
-
-  ngOnInit() {
+  constructor() {
     this.isVideoUploaded = false;
-    this.uploadedVideoUrl = '//fast.wistia.net/embed/iframe/djyx0apfhv?videoFoam=true';
+    this.apiSecret = '3b2f5f1795b5026114d535eff37b8eef7d5d9fdef6966ee1ae7ec702846d0b14';
+    this.isUploadingVideo = false;
+    this.videoUploadProgress = 0;
   }
 
+  ngAfterViewInit() {
+    let self = this;
+    function _onUploadCallback(e, data) {
+      data.formData = {
+        api_password: self.apiSecret
+      };
+      self.isUploadingVideo = true;
+      data.submit();
+    }
+
+    function _onProgressCallback(e, data) {
+      self.videoUploadProgress = data.loaded / data.total * 100;
+    }
+
+    function _onCompleteCallback(e, data) {
+      self.isUploadingVideo = false;
+      self.isVideoUploaded = true;
+      self.uploadedVideoUrl = self.getUploadedVideoUrl(data.result.hashed_id);
+    }
+
+    $('#file-upload').fileupload({
+      url: 'https://upload.wistia.com',
+      add: _onUploadCallback,
+      progressall: _onProgressCallback,
+      done: _onCompleteCallback
+    });
+  }
+
+  getUploadProgress() {
+    this.videoUploadProgress = this.videoUploadProgress > 100 ? 100 : this.videoUploadProgress;
+    return this.videoUploadProgress;
+  }
+
+  getUploadedVideoUrl(videoHash) {
+    return '//fast.wistia.net/embed/iframe/' + videoHash + '?videoFoam=true';
+  }
 }
